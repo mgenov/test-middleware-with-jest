22458fef11b62697cbf10eaf094461365e995db1
Miroslav Genov <mgenov@gmail.com>
2017-08-17 13:51:31 +0300

    Initial Commit of bug-showcase example

---
+++ src/axiosMiddleware.spec.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import axios from 'axios'
+import axiosMiddleware from 'redux-axios-middleware'
+import configureMockStore from 'redux-mock-store'
+import MockAdapter from 'axios-mock-adapter'
+
+const options = {
+  returnRejectedPromiseOnError: true,
+
+  interceptors: {
+    request: [
+      ({ getState, dispatch }, config) => {
+        config.headers['Authorization'] = 'Bearer ' + getState().access_token
+        return config
+      }
+    ],
+    response: [
+      {
+        success: ({ dispatch }, response) => {
+         return response
+        },
+        error: ({ dispatch, getSourceAction }, error) => {
+          return Promise.reject(error)
+        }
+      }
+    ]
+  }
+}
+
+const middleware = axiosMiddleware(axios, options)
+
+describe('axiosMiddleware', () => {
+  const client = axios.create({
+    responseType: 'json'
+  })
+
+  const mockAxiosClient = new MockAdapter(axios)
+  const mockStore = configureMockStore([middleware])
+  const mockAdapter = mockAxiosClient.adapter()
+
+  afterEach(() => {
+    mockAxiosClient.reset()
+  })
+
+  afterAll(() => {
+    mockAxiosClient.restore()
+  })
+
+  it('attaches authorization header on each request', () => {
+    expect.assertions(1);
+    let got
+
+    mockAxiosClient.onGet('/test').reply(config => {
+      got = config.headers['Authorization']
+      return [200]
+    })
+
+    const action = () => {
+      return {
+        type: 'LOAD',
+        payload: {
+          request: {
+            url: '/test'
+          }
+        }
+      }
+    }
+
+    const store = mockStore({ access_token: '::access_token::' })
+
+    return store.dispatch(action()).then(() => {
+      expect(got).toEqual('Bearer ::access_token::')
+    })
+  })
+  
+  
+  it('attaches authorization header on each request', () => {
+    expect.assertions(1);
+
+    let got
+
+    mockAxiosClient.onGet('/test').reply(config => {
+      got = config.headers['Authorization']
+      return [200]
+    })
+
+    const action = () => {
+      return {
+        type: 'ANOTHER_LOAD_ACTION',
+        payload: {
+          request: {
+            url: '/test'
+          }
+        }
+      }
+    }
+
+    const store = mockStore({ access_token: '::another_access_token::' })
+
+    return store.dispatch(action()).then(() => {
+      expect(got).toEqual('Bearer ::another_access_token::')
+    })
+  })
+})
